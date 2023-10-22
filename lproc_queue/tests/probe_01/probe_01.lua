1537d1fe8f3a610b56ecb9d4296abff46413f280
yrasik <stepanenkoyra@gmail.com>
2023-10-22 11:06:13 +0300

    Первый пример с семафором вроде завёлся...

--- lproc_queue/tests/probe_01/probe_01.lua
+++ lproc_queue/tests/probe_01/probe_01.lua
@@ -1,5 +1,4 @@
 #!/usr/bin/lua5.3
---cp1251
 
 
 local lproc_queue = require ("lproc_queue")
@@ -43,7 +42,6 @@ local function REPORT(level, fmt, ...)
 
   return
 end
-
 
 
 local proc_01_pattern = [[
@@ -104,11 +102,38 @@ end
 ]]
 
 
+local proc_04_pattern = [[
+local my_name = 'proc_04'
+local channel_in = <channel_in>
+local semaphore_in = <semaphore_in>
+
+print('--------- '..my_name..' -----------')
+local ret, message, queue_count
+
+while true do
+  ret, message = lproc_queue.sem_timedwait_ms(semaphore_in, 987)
+  if (ret == false) then
+    print('in '..my_name..': sem_timedwait_ms = false  message = \''..tostring(message)..'\'')
+  else
+    ret, message, queue_count = lproc_queue.queue_nb_pop(channel_in)
+    print('in '..my_name..': ret = '..tostring(ret)..'  message = \''..tostring(message)..'\'  queue_count = '..tostring(queue_count))
+  end
+end
+]]
+
+
+
 
 ------------------- main ---------------------
 --REPORT(MSG_INFO, "channel_02 = 0x%016X", channel_02)
 
 REPORT(MSG_INFO, lproc_queue.version())
+
+
+local ENABLE_proc_01 = true
+local ENABLE_proc_02 = true
+local ENABLE_proc_03 = true
+local ENABLE_proc_04 = true
 
 
 local debug_info = lproc_queue.debug_mode('0b1000_0000_0000_0000_0000_0000_0001_1110');
@@ -130,10 +155,35 @@ local proc_03 = string.gsub(proc_03_pattern, "<channel_in>", string.format("0x%0
 proc_03 = string.gsub(proc_03, "<channel_out>", string.format("0x%016X", channel__from_proc_03))
 proc_03 = string.gsub(proc_03, "<destination>", my_name)
 
+local channel__to_proc_04 = lproc_queue.queue_create()
+local semaphore__to_proc_04 = lproc_queue.sem_create()
+local proc_04 = string.gsub(proc_04_pattern, "<channel_in>", string.format("0x%016X", channel__to_proc_04))
+proc_04 = string.gsub(proc_04, "<semaphore_in>", string.format("0x%016X", semaphore__to_proc_04))
 
-local ret = lproc_queue.proc_start(proc_01)
-local ret = lproc_queue.proc_start(proc_02)
-local ret = lproc_queue.proc_start(proc_03)
+
+
+
+
+
+
+
+
+if ENABLE_proc_01 then
+  local ret = lproc_queue.proc_start(proc_01)
+end
+
+if ENABLE_proc_02 then
+  local ret = lproc_queue.proc_start(proc_02)
+end
+
+if ENABLE_proc_03 then
+  local ret = lproc_queue.proc_start(proc_03)
+end
+
+if ENABLE_proc_04 then
+  local ret = lproc_queue.proc_start(proc_04)
+end
+
 
 
 local ret, message, size
@@ -141,12 +191,26 @@ local i = 0
 
 while true do
   lproc_queue.delay_ms(498)
-  lproc_queue.queue_push(channel__to_proc_01, string.format("{send_from = '%s', send_to = 'proc_01', message = '%d'}", my_name, i))
-  lproc_queue.queue_push(channel__to_proc_02, string.format("{send_from = '%s', send_to = 'proc_02', message = '%d'}", my_name, i))
-  lproc_queue.queue_push(channel__to_proc_03, string.format("{send_from = '%s', send_to = 'proc_03', message = '%d'}", my_name, i))
 
-  ret, message, size = lproc_queue.queue_nb_pop(channel__from_proc_03)
-  print('in '..my_name..': ret = '..tostring(ret)..'  message = \''..tostring(message)..'\'  size = '..tostring(size))
+  if ENABLE_proc_01 then
+    lproc_queue.queue_push(channel__to_proc_01, string.format("{send_from = '%s', send_to = 'proc_01', message = '%d'}", my_name, i))
+  end
+
+  if ENABLE_proc_02 then
+    lproc_queue.queue_push(channel__to_proc_02, string.format("{send_from = '%s', send_to = 'proc_02', message = '%d'}", my_name, i))
+  end
+
+  if ENABLE_proc_03 then
+    lproc_queue.queue_push(channel__to_proc_03, string.format("{send_from = '%s', send_to = 'proc_03', message = '%d'}", my_name, i))
+    ret, message, size = lproc_queue.queue_nb_pop(channel__from_proc_03)
+    print('in '..my_name..': ret = '..tostring(ret)..'  message = \''..tostring(message)..'\'  size = '..tostring(size))
+  end
+
+  if ENABLE_proc_04 then
+    lproc_queue.queue_push(channel__to_proc_04, string.format("{send_from = '%s', send_to = 'proc_04', message = '%d'}", my_name, i))
+    lproc_queue.sem_post(semaphore__to_proc_04)
+  end
+
 
   i = i + 1
 end
