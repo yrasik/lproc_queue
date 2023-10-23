917bd51abf1e660c6ddd5cd025bfe0267ea1e96e
yrasik <stepanenkoyra@gmail.com>
2023-10-23 21:48:42 +0300

    Удалось собрать из buildroot

--- lproc_queue.mk
+++ lproc_queue.mk
@@ -11,13 +11,13 @@ LPROC_QUEUE_SITE_METHOD = local
 LPROC_QUEUE_DEPENDENCIES = lua
 
 
-define LUAMODBUS_CONSOLE_BUILD_CMDS
+define LPROC_QUEUE_BUILD_CMDS
     $(MAKE) CC="$(TARGET_CC)" LD="$(TARGET_LD)" -C $(@D)
     #$(TARGET_MAKE_ENV) $(MAKE1) -C $(@D)
 endef
 
-define LUAMODBUS_CONSOLE_INSTALL_TARGET_CMDS
-    $(INSTALL) -D -m 0755 $(@D)/lproc_queue.so $(TARGET_DIR)/usr/lib/lua/5.4
+define LPROC_QUEUE_INSTALL_TARGET_CMDS
+    $(INSTALL) -D -m 0755 $(@D)/build/lproc_queue.so $(TARGET_DIR)/usr/lib/lua/5.3
     #$(TARGET_MAKE_ENV) $(MAKE1) -C $(@D) DESTDIR=$(STAGING_DIR) LDCONFIG=true install
 endef
 
